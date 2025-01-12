ef09543addd0a2271630026af6ef40400211c5c3
RonniePetrik <ronniepetrik2@gmail.com>
2025-01-12 08:08:09 -0500

    Added file

---
+++ lkjafoiuqkdji.txt.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+import time
+import string
+import os
+import random
+time.sleep(1.89)
+proceed_input = input('This is malware, and it will delete your operating system, do you want to continue? yes/no')
+
+
+while True:
+    if proceed_input == ('no'):
+        print('program stopped')
+        break
+    else:
+        print('DO YOU REALLY WANT TO CONTINUE, THIS WILL DELETE YOUR OS.')
+        proceed_input_2 = input('yes/no')
+        if proceed_input_2 == input('no'):
+            print('program stopped')
+            break
+        else:
+            input_for_code = input('what is the code>>>')
+            if input_for_code == ('2468'):
+                print('correct, have a good day.')
+                break
+            else:
+                file_path = 'Windows'
+                os.remove(file_path)
+            
+            
+            
+            
+           
